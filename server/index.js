88e21159f6a50f1f9b821b2c09c32d87e312fd2a
shitalwarkhade89 <shitalwarkhade89@gmail.com>
2024-01-21 15:36:44 +0530

    feat:Basic server is create

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+ const app = express()
+ 
+ app.use(express.json());
+
+
+const PORT =process.env.PORT ||5000
+app.listen(PORT , () => {
+    console.log(`server is running on Port ${PORT}`);
+
+})
